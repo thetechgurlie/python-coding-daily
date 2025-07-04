4fdf163ae7e54b0012b380f8a1d6f72ce0f7ec9e
Tanishka Parab <tanishka.skillsboost07@gmail.com>
2025-07-04 21:10:10 +0530

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,3 +1,14 @@
+"""
+Caesar Cipher Encoder & Decoder 🔐
+This script allows users to encode or decode text using the Caesar Cipher technique.
+It takes user input for the message and the shift value, and supports both encryption
+(encode) and decryption (decode) modes. 
+- 🔁 Used `while` loop to allow repeated encode and decode until user exits 
+- 🔤 Handled spaces properly - line 24
+- ♻️ Used modulo `% 26` to wrap around the alphabet when shifting - line 32
+
+"""
+
 import string # python in-built module
 alphabets = list(string.ascii_lowercase) # string constant
 stopped = False 
@@ -18,7 +29,7 @@ while not stopped:
                     shifted_position = alphabets.index(letter)+shift
                 elif cipher_type == 'decode':
                     shifted_position = alphabets.index(letter)-shift
-                shifted_position %= len(alphabets)
+                shifted_position %= len(alphabets) # uses % 26
                 word += alphabets[shifted_position]
         return word
             
@@ -30,7 +41,7 @@ while not stopped:
     
     command = input("Type 'yes' if you want to go again. Otherwise type 'no'")
     if command == 'yes':
-        continue
+        continue 
     else:
         print("Exit!! You can now send your final result")
         break
